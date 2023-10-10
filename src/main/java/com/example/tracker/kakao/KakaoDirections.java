cfb2ca26ca2ea7262bb50c65f10a3774f1437658
김민규 <gimmingyu@gimmingyuui-MacBookAir.local>
2023-10-10 22:45:39 +0900

    Separate and optimize methods, add documentation comments

--- src/main/java/com/example/tracker/kakao/KakaoDirections.java
+++ src/main/java/com/example/tracker/kakao/KakaoDirections.java
@@ -6,6 +6,7 @@ import java.util.List;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.HttpStatus;
 import org.apache.http.util.EntityUtils;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
@@ -26,8 +27,6 @@ public class KakaoDirections {
 
     @Value("${rest-api.key}")
     private String authorizationKey;
-
-    private static final int HTTP_OK = 200;
 
     private final CloseableHttpClient httpClient;
     private final ObjectMapper objectMapper;
@@ -55,6 +54,12 @@ public class KakaoDirections {
 
     /**
      * API 주소를 구성합니다.
+     *
+     * @param startX 출발지의 x 좌표
+     * @param startY 출발지의 y 좌표
+     * @param endX   도착지의 x 좌표
+     * @param endY   도착지의 y 좌표
+     * @return 완성된 API 주소
      */
     private String buildApiAddress(Double startX, Double startY, Double endX, Double endY) {
         return routeApiUrl
@@ -64,7 +69,10 @@ public class KakaoDirections {
     }
 
     /**
-     * HTTP GET 요청 객체를 생성합니다.
+     * API 주소를 기반으로 HTTP GET 요청 객체를 생성합니다.
+     *
+     * @param apiAddress 호출할 API 주소
+     * @return 생성된 HTTP GET 요청 객체
      */
     private HttpGet buildHttpGetRequest(String apiAddress) {
         HttpGet httpGet = new HttpGet(apiAddress);
@@ -73,10 +81,14 @@ public class KakaoDirections {
     }
 
     /**
-     * HTTP 응답을 처리하여 좌표 목록을 반환합니다.
+     * HTTP 응답을 처리하여 길찾기의 결과로 얻은 좌표 목록을 반환합니다.
+     *
+     * @param response HTTP 요청에 대한 응답 객체
+     * @return 길찾기 결과의 좌표 리스트
+     * @throws Exception JSON 파싱이나 HTTP 처리 중 발생할 수 있는 예외
      */
     private List<Info.Coordinate> handleHttpResponse(CloseableHttpResponse response) throws Exception {
-        if (response.getStatusLine().getStatusCode() != HTTP_OK) {
+        if (response.getStatusLine().getStatusCode() != HttpStatus.SC_OK) {
             throw new RuntimeException("API 응답 오류: " + response.getStatusLine().getStatusCode());
         }
 
@@ -86,7 +98,10 @@ public class KakaoDirections {
     }
 
     /**
-     * 경로 정보로부터 좌표 목록을 추출하고 평균 시간을 분배합니다.
+     * 주어진 경로 정보에서 모든 좌표를 추출합니다.
+     *
+     * @param route 길찾기 API로부터 받은 경로 정보
+     * @return 길찾기 결과의 좌표 리스트
      */
     private List<Info.Coordinate> extractCoordinates(Info.Route route) {
         List<Info.Coordinate> coordinates = new ArrayList<>();
@@ -102,7 +117,10 @@ public class KakaoDirections {
     }
 
     /**
-     * 전체 좌표의 수를 계산합니다.
+     * 경로의 전체 좌표 수를 계산합니다.
+     *
+     * @param route 길찾기 API로부터 받은 경로 정보
+     * @return 전체 좌표 수
      */
     private int calculateTotalCoordinates(Info.Route route) {
         int totalCoordinates = 0;
@@ -115,7 +133,11 @@ public class KakaoDirections {
     }
 
     /**
-     * 출발지 좌표를 추가합니다.
+     * 경로의 출발지 좌표를 추가합니다.
+     *
+     * @param route       길찾기 API로부터 받은 경로 정보
+     * @param coordinates 좌표를 추가할 리스트
+     * @param duration    좌표별 평균 소요 시간
      */
     private void addOriginCoordinate(Info.Route route, List<Info.Coordinate> coordinates, int duration) {
         Info.Coordinate origin = route.getSummary().getOrigin();
@@ -123,7 +145,11 @@ public class KakaoDirections {
     }
 
     /**
-     * 각 섹션의 도로 정보로부터 좌표를 추출하여 목록에 추가합니다.
+     * 경로의 각 섹션에서 도로의 좌표를 추출하고 목록에 추가합니다.
+     *
+     * @param route       길찾기 API로부터 받은 경로 정보
+     * @param coordinates 좌표를 추가할 리스트
+     * @param duration    좌표별 평균 소요 시간
      */
     private void addSectionCoordinates(Info.Route route, List<Info.Coordinate> coordinates, int duration) {
         for (Info.Section section : route.getSections()) {
@@ -140,7 +166,11 @@ public class KakaoDirections {
     }
 
     /**
-     * 도착지 좌표를 추가합니다.
+     * 경로의 도착지 좌표를 추가합니다.
+     *
+     * @param route       길찾기 API로부터 받은 경로 정보
+     * @param coordinates 좌표를 추가할 리스트
+     * @param duration    좌표별 평균 소요 시간
      */
     private void addDestinationCoordinate(Info.Route route, List<Info.Coordinate> coordinates, int duration) {
         Info.Coordinate destination = route.getSummary().getDestination();
--- src/main/java/com/example/tracker/service/impl/DeliveryServiceImpl.java
+++ src/main/java/com/example/tracker/service/impl/DeliveryServiceImpl.java
@@ -10,6 +10,7 @@ import javax.transaction.Transactional;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.HttpStatus;
 import org.apache.http.util.EntityUtils;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
@@ -29,15 +30,31 @@ import lombok.extern.slf4j.Slf4j;
 @Service
 public class DeliveryServiceImpl implements DeliveryService {
 
+    private static final String AUTHORIZATION_PREFIX = "KakaoAK ";
+    private static final String QUERY_PARAM = "?query=";
+
     @Value("${rest-api.key}")
     private String authorizationKey;
-    private static final int HTTP_OK = 200;
+
+    @Value("${converter-api.url}")
+    private String apiUrl;
+
+    @Value("${batch.size}")
+    private int batchSize;
 
     private final DeliveryRepository deliveryRepository;
     private final CloseableHttpClient httpClient;
     private final ObjectMapper objectMapper;
     private final EntityManager em;
 
+    /**
+     * 주어진 의존성들로 {@code DeliveryServiceImpl} 객체를 생성합니다.
+     *
+     * @param deliveryRepository 배송 정보 관련 레포지토리
+     * @param httpClient         HTTP 클라이언트
+     * @param objectMapper       JSON 매퍼
+     * @param em                 엔터티 매니저
+     */
     public DeliveryServiceImpl(
             DeliveryRepository deliveryRepository,
             CloseableHttpClient httpClient,
@@ -50,93 +67,122 @@ public class DeliveryServiceImpl implements DeliveryService {
     }
 
     /**
-     * 주소를 좌표로 변환하는 메서드.
+     * 주어진 주소를 좌표로 변환합니다.
      *
      * @param address 변환할 주소
-     * @return 좌표를 담은 Double 배열. 실패 시 null 반환.
+     * @return 변환된 좌표. 변환 실패 시 null.
      */
     @Override
     public Double[] converter(String address) {
-        Double[] coordinate = null;
-        String apiUrl = "https://dapi.kakao.com/v2/local/search/address.json";
-
         try {
-            // 주소를 UTF-8로 인코딩합니다.
-            String encodedAddress = URLEncoder.encode(address, StandardCharsets.UTF_8.toString());
-            String apiAddress = apiUrl + "?query=" + encodedAddress;
-
-            // HttpGet 객체를 생성하고 헤더를 설정합니다.
-            HttpGet httpGet = new HttpGet(apiAddress);
-            httpGet.setHeader("Authorization", "KakaoAK " + authorizationKey);
-
-            // HTTP 요청을 실행합니다.
-            try (CloseableHttpResponse response = httpClient.execute(httpGet)) {
-                if (response.getStatusLine().getStatusCode() == HTTP_OK) {
-                    String responseBody = EntityUtils.toString(response.getEntity());
-                    JsonNode jsonNode = objectMapper.readTree(responseBody);
-                    coordinate = new Double[2];
-
-                    // JSON 응답에서 좌표를 추출합니다.
-                    if (jsonNode.has("documents")) {
-                        JsonNode documents = jsonNode.get("documents");
-                        if (documents.isArray() && documents.size() > 0) {
-                            JsonNode firstDocument = documents.get(0);
-                            if (firstDocument.has("x") && firstDocument.has("y")) {
-                                coordinate[0] = firstDocument.get("x").asDouble();
-                                coordinate[1] = firstDocument.get("y").asDouble();
-                            }
-                        }
-                    }
-                }
-            }
+            String responseBody = executeHttpRequest(address);
+            return extractCoordinatesFromResponse(responseBody);
         } catch (Exception e) {
             log.error("주소를 좌표로 변환하는 과정에서 오류가 발생했습니다.", e);
             return null;
         }
-        return coordinate;
     }
 
     /**
-     * 배송 정보를 저장하는 메서드.
+     * HTTP 요청을 실행하고 응답 본문을 반환합니다.
      *
-     * @param deliveryDto 저장할 배송 정보의 DTO 객체
+     * @param address 변환할 주소
+     * @return HTTP 응답 본문
+     * @throws Exception HTTP 요청 중 발생하는 예외
+     */
+    private String executeHttpRequest(String address) throws Exception {
+        String encodedAddress = URLEncoder.encode(address, StandardCharsets.UTF_8.toString());
+        String apiAddress = apiUrl + QUERY_PARAM + encodedAddress;
+
+        HttpGet httpGet = new HttpGet(apiAddress);
+        httpGet.setHeader("Authorization", AUTHORIZATION_PREFIX + authorizationKey);
+
+        try (CloseableHttpResponse response = httpClient.execute(httpGet)) {
+            if (response.getStatusLine().getStatusCode() == HttpStatus.SC_OK) {
+                return EntityUtils.toString(response.getEntity());
+            }
+            return null;
+        }
+    }
+
+    /**
+     * 응답 본문에서 좌표를 추출합니다.
+     *
+     * @param responseBody HTTP 응답 본문
+     * @return 추출된 좌표 배열
+     * @throws Exception JSON 파싱 중 발생하는 예외
+     */
+    private Double[] extractCoordinatesFromResponse(String responseBody) throws Exception {
+        JsonNode jsonNode = objectMapper.readTree(responseBody);
+
+        if (jsonNode.has("documents")) {
+            JsonNode documents = jsonNode.get("documents");
+            if (documents.isArray() && documents.size() > 0) {
+                JsonNode firstDocument = documents.get(0);
+                if (firstDocument.has("x") && firstDocument.has("y")) {
+                    return new Double[] { firstDocument.get("x").asDouble(), firstDocument.get("y").asDouble() };
+                }
+            }
+        }
+        return null;
+    }
+
+    /**
+     * 배송 정보를 데이터베이스에 저장합니다.
+     *
+     * @param deliveryDto 저장할 배송 정보 DTO
      */
     @Override
     public void saveDelivery(DeliveryDto deliveryDto) {
-        DeliveryEntity deliveryEntity = DeliveryEntity.builder()
+        DeliveryEntity deliveryEntity = convertDtoToEntity(deliveryDto);
+        deliveryRepository.save(deliveryEntity);
+    }
+
+    /**
+     * DTO를 엔터티 객체로 변환합니다.
+     *
+     * @param deliveryDto 변환할 배송 정보 DTO
+     * @return 변환된 배송 정보 엔터티 객체
+     */
+    private DeliveryEntity convertDtoToEntity(DeliveryDto deliveryDto) {
+        return DeliveryEntity.builder()
                 .startAddress(deliveryDto.getStartAddress())
                 .endAddress(deliveryDto.getEndAddress())
                 .name(deliveryDto.getName())
                 .productName(deliveryDto.getProductName())
                 .item(deliveryDto.getItem())
                 .build();
-        deliveryRepository.save(deliveryEntity);
     }
 
     /**
-     * 대량의 경로 좌표 데이터를 데이터베이스에 저장합니다.
+     * 주어진 좌표 리스트를 데이터베이스에 일괄 저장합니다.
      *
-     * @param coordinates 저장할 경로 좌표 리스트
+     * @param coordinates 저장할 좌표 리스트
      */
     @Transactional
     public void saveCoordinates(List<Info.Coordinate> coordinates) {
-        int batchSize = 50;
-
         for (int i = 0; i < coordinates.size(); i++) {
-            Info.Coordinate coordinate = coordinates.get(i);
-
-            CoordinateEntity entity = CoordinateEntity.builder()
-                    .name(coordinate.getName())
-                    .x(coordinate.getX())
-                    .y(coordinate.getY())
-                    .duration(coordinate.getDuration())
-                    .build();
-            em.persist(entity);
+            persistCoordinate(coordinates.get(i));
 
             if (i % batchSize == 0 || i == coordinates.size() - 1) {
                 em.flush();
                 em.clear();
             }
         }
+    }
+
+    /**
+     * 주어진 좌표를 데이터베이스에 저장합니다.
+     *
+     * @param coordinate 저장할 좌표 정보
+     */
+    private void persistCoordinate(Info.Coordinate coordinate) {
+        CoordinateEntity entity = CoordinateEntity.builder()
+                .name(coordinate.getName())
+                .x(coordinate.getX())
+                .y(coordinate.getY())
+                .duration(coordinate.getDuration())
+                .build();
+        em.persist(entity);
     }
 }
